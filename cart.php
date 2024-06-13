d75bd2bfcf9fb096f2b2591861e1b3859961f332
Jerich Elano <jerichelano@gmail.com>
2024-06-13 19:36:21 +0800

    Fixed the bug in update quality function; Changed the condition in add to cart function (Add 1 product every click of add to cart button)

--- cart.php
+++ cart.php
@@ -20,13 +20,25 @@ if(isset($_GET['delete_all'])){
     mysqli_query($conn, "DELETE FROM `cart` WHERE account_id = '$account_id'") or die('query failed');
     header('location:cart.php');
 };
-// fix the bug here -------------------------------------(it changes every quantity of the products that are in the same cart)
-if(isset($_POST['update_quantity'])){
-    $cart_id = $_POST['cart_id'];
+
+if (isset($_POST['update_quantity'])) {
+    $cart_account_id = $_POST['account_id'];
+    $cart_pid = $_POST['pid'];
     $cart_quantity = $_POST['cart_quantity'];
-    mysqli_query($conn, "UPDATE `cart` SET quantity = '$cart_quantity' WHERE id = '$cart_id'") or die('query failed');
-    $_SESSION['messages'][] = 'Cart quantity updated!';
+
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare("UPDATE `cart` SET quantity = ? WHERE account_id = ? AND pid = ?");
+    $stmt->bind_param('isi', $cart_quantity, $cart_account_id, $cart_pid);
+
+    if ($stmt->execute()) {
+        $_SESSION['messages'][] = 'Cart quantity updated!';
+    } else {
+        die('Query failed: ' . $stmt->error);
+    }
+
+    $stmt->close();
 }
+
 
 ?>
 
@@ -63,14 +75,15 @@ if(isset($_POST['update_quantity'])){
                     if(mysqli_num_rows($select_cart) > 0){
                         while($fetch_cart = mysqli_fetch_assoc($select_cart)){
                 ?>
-                <div  class="box">
+                <div class="box">
                     <a href="cart.php?delete=<?php echo $fetch_cart['id']; ?>" class="fas fa-times" onclick="return confirm('delete this from cart?');"></a>
-                    <a href="view_page.php?pid=<?php echo $fetch_cart['pid']; ?>" class="fas fa-eye"></a>
+                    <!-- <a href="view_page.php?pid=<?php echo $fetch_cart['pid']; ?>" class="fas fa-eye"></a> -->
                     <img src="assets/img/uploaded-img/<?php echo $fetch_cart['image']; ?>" alt="" class="image">
                     <div class="name"><?php echo $fetch_cart['name']; ?></div>
                     <div class="price">P<?php echo $fetch_cart['price']; ?></div>
                     <form action="" method="post">
-                        <input type="hidden" value="<?php echo $fetch_cart['id']; ?>" name="cart_id">
+                        <input type="hidden" value="<?php echo $fetch_cart['account_id']; ?>" name="account_id">
+                        <input type="hidden" value="<?php echo $fetch_cart['pid']; ?>" name="pid">
                         <input type="number" min="1" value="<?php echo $fetch_cart['quantity']; ?>" name="cart_quantity" class="qty">
                         <input type="submit" value="update" class="btn" name="update_quantity">
                     </form>
